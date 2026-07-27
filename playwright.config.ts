551912fbd1d9cc393f8030651cea3d30d67caa3d
NgocDoMinh <dmn@aod.vn>
2026-07-27 08:34:15 +0200

    remove firefox

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,9 +46,9 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ],
 });
